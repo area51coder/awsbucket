83db104a54119aad88a463d3656505dbf2c4aff9
area51coder <85442349+area51coder@users.noreply.github.com>
2022-08-23 11:34:43 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ region = "us-east-2"
 }
 
 resource "aws_s3_bucket" "terraform_state" {
-bucket = "terraform-up-and-running-state"
+bucket = "terraform-up-and-running-state-area51"
 # Prevent accidental deletion of this S3 bucket
 lifecycle {
 prevent_destroy = true
